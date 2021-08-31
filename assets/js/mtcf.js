78fdc273b7a44349d8c1d97f95c8d6cbe64dcb2d
yoyodax <rayapur@yahoo.com>
2021-08-31 19:32:49 +1000

    New website 20210831

--- assets/js/mtcf.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    //$(document).snowfall({flakeCount : 800, maxSpeed : 2, maxSize : 5});
-
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox();
-    });
-});
